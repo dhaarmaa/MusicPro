db973ccc5fce7e96f20a0e4f874d18de5dd22841
Gabro22 <gabrielons2010@gmail.com>
2023-05-12 22:18:10 -0400

    ruteo

---
+++ src/applicacion/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from '../views/home';
+import Page2 from '../views/page2';
+
+const Router = () => (
+    <BrowserRouter>
+        <Routes>   
+            <Route path='/' element={<Home/>} />
+            <Route path="/page2/" element={<Page2/>} />
+             { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}    
+            <Route path="*" element={<div>404</div> } />
+        </Routes>
+    </BrowserRouter>
+);
+
+export default Router;
---
+++ src/views/page2/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Page2 = () => <div>page 2</div>;
+export default Page2;
