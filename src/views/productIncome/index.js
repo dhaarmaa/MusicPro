0e284b24d7cc8d4408a781aba2d52044d0a5d0c5
dhaarmaa <dharma.herrera@gmail.com>
2023-05-12 23:44:09 -0400

    esqueleto del formulario de ingreso de productos

--- src/views/productIncome/index.js
+++ src/views/productIncome/index.js
@@ -4,6 +4,21 @@ import { Link } from 'react-router-dom';
 const ProductIncome = () => 
     <div>
         <h1>Entrada de producto</h1>
+  
+
+        <div>
+           <h3>Nombre</h3> 
+           <input type="text"/>
+
+           <h3>codigo</h3>
+           <input type="text"/>
+
+           <h3>stock</h3>
+           <input type="number"/>
+
+           <button>Ingresar</button>
+
+        </div>
         <button><Link to={`/`}>atras</Link></button>
     </div>;
 export default ProductIncome;
